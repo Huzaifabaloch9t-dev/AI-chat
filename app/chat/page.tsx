f81a2e12727ceac613bbdbff467e4823829e403c
huzaifa baloch <huzaifa.baloch.9t@gmail.com>
2026-02-10 19:33:46 -0800

    update page of chat

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -48,7 +48,6 @@ export default function ChatPage() {
         🤖 AI Chat
       </div>
 
-      {/* Messages */}
       <div className="flex-1 overflow-y-auto p-4 space-y-3">
         {messages.map((msg, i) => (
           <div
